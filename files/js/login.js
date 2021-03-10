f4f4231d62c26d6bb44eda1031ff80417b4d134a
shvedoff1985 <support@shvedoff.ru>
2021-03-10 15:35:49 +0300

    sms

--- files/js/login.js
+++ files/js/login.js
@@ -19,12 +19,23 @@ function login() {
 	$("#overlay").css("height", windowheight-55);
    	$(".PIN").css("margin-top", (windowheight-$(".PIN").height())/2 - 55-72 );
 
-    $.get("https://smsc.ru/sys/send.php?login=shvedoff1985&psw=b12132ff02dd8e140df8fa5c59c23df1&phones="+$("#phone-mask").val()+"&mes=code&call=1")
-        .done(function(data) {
-            alert('123');
-            alert(data);
-        });
+    // 1. Создаём новый объект XMLHttpRequest
+    var xhr = new XMLHttpRequest();
 
+// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+    xhr.open('GET', 'https://smsc.ru/sys/send.php?login=shvedoff1985&psw=b12132ff02dd8e140df8fa5c59c23df1&phones='+$("#phone-mask").val()+'&mes=code&call=1&fmt=3', false);
+
+// 3. Отсылаем запрос
+    xhr.send();
+
+// 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status != 200) {
+        // обработать ошибку
+        alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+    } else {
+        // вывести результат
+        alert( xhr.responseText ); // responseText -- текст ответа.
+    }
 }
 
 function CloseOverlay() {
