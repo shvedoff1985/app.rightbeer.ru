a4bb12391cd9b4e477849531a61bb46957290530
pok699-1 <maksf2071@gmail.com>
2021-03-12 15:13:20 +0300

    Обновление карты

--- files/js/main.js
+++ files/js/main.js
@@ -6,6 +6,10 @@ if (localStorage.account == "login") {
     User=true;
     Page='profile';
     profile();
+}else{
+    User=false;
+    Page='login';
+    login();
 }
 
 //Главный прелоудер
@@ -14,6 +18,9 @@ if(!First_exit) {setTimeout(function(){$("#preload-main").css("display", "none")
 
 $(document).ready ( function(){
     if (!User && Page == 'login') {
+        //Атрибут Page для layout
+        $('#layout').removeAttr('page');
+        $('#layout').attr('page', 'login');
 
         //Название страницы
         $(".name-layout .name").html('Авторизация');
@@ -107,6 +114,11 @@ function login() {
     Page = 'login';
 
     if (!User && Page == 'login') {
+        //Атрибут Page для layout
+        $('#layout').removeAttr('page');
+        $('#layout').attr('page', 'login');
+
+        $("metajs").html('');
 
         //Название страницы
         $(".name-layout .name").html('Авторизация');
@@ -191,14 +203,27 @@ function login() {
         var phoneMask = IMask(document.getElementById('phone-mask'), {mask: '+7 (000) 000-00-00'});
 		var regExpMask = IMask(document.getElementById('pin-mask'), {mask: '0-0-0-0'});
     }
+
+    //Нижний юлок
+    $(".button").removeClass('active');
+    $("#tabbar .login.profile").html(
+    '<div onclick="login()" class="button active">\
+        <div class="icon"><i class="far fa-user-circle"></i></div>\
+        <div class="name">Войти</div>\
+    <div>'
+    );
 }
 
 
 function profile() {
     Page = 'profile';
+    //Атрибут Page для layout
+    $('#layout').removeAttr('page');
+    $('#layout').attr('page', 'profile');
+
+    $("metajs").html('');
 
     //Название страницы
-    $(".name-layout .name").html('Профиль');
     $(".name-layout .name").html('Профиль');
 
     //Загрузка контента
@@ -208,18 +233,237 @@ function profile() {
     </div>'
     );
 
+    //Нижний юлок
     $(".button").removeClass('active');
-    $("#tabbar .login").html(
-    '<div onclick="profile()" class="button active">\
-        <div class="icon"><i class="fas fa-barcode"></i></div>\
-        <div class="name">Профиль</div>\
-    <div>'
+    if (localStorage.account == "login") {
+        $("#tabbar .login.profile").html(
+        '<div onclick="profile()" class="button">\
+            <div class="icon"><i class="fas fa-barcode"></i></div>\
+            <div class="name">Профиль</div>\
+        <div>'
+        );
+        $(".name-layout .exit").html('<i class="fas fa-sign-out-alt"></i>');
+    }else{
+        $("#tabbar .login.profile").html(
+        '<div onclick="login()" class="button">\
+            <div class="icon"><i class="far fa-user-circle"></i></div>\
+            <div class="name">Войти</div>\
+        </div>'
+        );
+    }
+    $("#tabbar .login .button").addClass('active');
+}
+
+function news() {
+    Page = 'news';
+    //Атрибут Page для layout
+    $('#layout').removeAttr('page');
+    $('#layout').attr('page', 'news');
+
+    $("metajs").html('');
+
+    //Название страницы
+    $(".name-layout .name").html('Новости');
+    //Загрузка контента
+    $("#layout").html(
+    '<div class="logo">\
+        <img src="files/images/logo-light.png" width="200">\
+    </div>'
     );
 
 
-    $("#tabbar .login").addClass('profile');
-    $("#tabbar .login .button").addClass('active');
-    $(".button .profile").removeClass('login');
-
-
+    //Нижний юлок
+    $(".button").removeClass('active');
+    if (localStorage.account == "login") {
+        $("#tabbar .login.profile").html(
+        '<div onclick="profile()" class="button">\
+            <div class="icon"><i class="fas fa-barcode"></i></div>\
+            <div class="name">Профиль</div>\
+        <div>'
+        );
+        $(".name-layout .exit").html('<i class="fas fa-sign-out-alt"></i>');
+    }else{
+        $("#tabbar .login.profile").html(
+        '<div onclick="login()" class="button">\
+            <div class="icon"><i class="far fa-user-circle"></i></div>\
+            <div class="name">Войти</div>\
+        </div>'
+        );
+    }
+    $("#tabbar .news .button").addClass('active');
 }
+
+
+
+function stocks() {
+    Page = 'stocks';
+    //Атрибут Page для layout
+    $('#layout').removeAttr('page');
+    $('#layout').attr('page', 'stocks');
+
+    $("metajs").html('');
+
+    //Название страницы
+    $(".name-layout .name").html('Акции');
+    //Загрузка контента
+    $("#layout").html(
+    '<div class="logo">\
+        <img src="files/images/logo-light.png" width="200">\
+    </div>'
+    );
+
+
+    //Нижний юлок
+    $(".button").removeClass('active');
+    if (localStorage.account == "login") {
+        $("#tabbar .login.profile").html(
+        '<div onclick="profile()" class="button">\
+            <div class="icon"><i class="fas fa-barcode"></i></div>\
+            <div class="name">Профиль</div>\
+        <div>'
+        );
+
+        $(".name-layout .exit").html('<i class="fas fa-sign-out-alt"></i>');
+    }else{
+        $("#tabbar .login.profile").html(
+        '<div onclick="login()" class="button">\
+            <div class="icon"><i class="far fa-user-circle"></i></div>\
+            <div class="name">Войти</div>\
+        </div>'
+        );
+    }
+    $("#tabbar .stocks .button").addClass('active');
+}
+
+
+function map() {
+    Page = 'map';
+    //Атрибут Page для layout
+    $('#layout').removeAttr('page');
+    $('#layout').attr('page', 'map');
+
+    //Название страницы
+    $(".name-layout .name").html('Карты');
+    //Загрузка контента
+    $("#layout").html(
+    '<div id="map" style="width: 100%; height: 100%"></div>'
+    );
+    $("metajs").html(
+    '<script src="https://api-maps.yandex.ru/2.1/?apikey=4589713e-a8e5-4019-bd5a-e4de5fb76b1d&lang=ru_RU" type="text/javascript"></script>'
+    );
+
+
+
+    //Функции map.js
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+
+        options = {
+            maximumAge: 30000,
+            timeout: 5000,
+            enableHighAccuracy: true
+        };
+
+        var onSuccess = function(position) {
+            ymaps.ready(init);
+
+            function init() {
+                var geolocation = ymaps.geolocation,
+
+                    myMap = new ymaps.Map('map', {
+                        center: [position.coords.latitude, position.coords.longitude],
+                        zoom: 17
+                    }, {
+                        searchControlProvider: 'yandex#search'
+                    });
+
+                    placemark1 = new ymaps.Placemark([51.648817, 39.195831], {
+                        hintContent: "Воронеж, 20 летия октября, 66"
+                    }, {
+                        preset: "islands#greenShoppingIcon"
+                    });
+
+                    placemark2 = new ymaps.Placemark([51.693753, 39.144978], {
+                        hintContent: "Воронеж, 45 СтрДивизии, 226"
+                    }, {
+                        preset: "islands#greenShoppingIcon"
+                    });
+
+                    placemark3 = new ymaps.Placemark([position.coords.latitude, position.coords.longitude], {
+                        hintContent: "Воронеж, 45 СтрДивизии, 226"
+                    }, {
+                        preset: "islands#geolocationIcon"
+                    });
+
+                myMap.geoObjects.add(placemark1).add(placemark2).add(placemark3)
+            }
+        };
+
+
+
+        function onError(error) {
+            alert('????');
+            alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+    }
+
+
+    //Нижний юлок
+    $(".button").removeClass('active');
+    if (localStorage.account == "login") {
+        $("#tabbar .login.profile").html(
+        '<div onclick="profile()" class="button">\
+            <div class="icon"><i class="fas fa-barcode"></i></div>\
+            <div class="name">Профиль</div>\
+        <div>'
+        );
+        $(".name-layout .exit").html('<i class="fas fa-sign-out-alt"></i>');
+    }else{
+        $("#tabbar .login.profile").html(
+        '<div onclick="login()" class="button">\
+            <div class="icon"><i class="far fa-user-circle"></i></div>\
+            <div class="name">Войти</div>\
+        </div>'
+        );
+    }
+    $("#tabbar .map .button").addClass('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.exit').click(function() {
+    $('.exit').html('');
+    delete localStorage.account;
+    User=false;
+    Page='login';
+    $("#tabbar .login.profile").html(
+    '<div onclick="login()" class="button">\
+        <div class="icon"><i class="far fa-user-circle"></i></div>\
+        <div class="name">Войти</div>\
+    </div>'
+    );
+    $("#tabbar .news .button").addClass('active');
+    login();
+});
